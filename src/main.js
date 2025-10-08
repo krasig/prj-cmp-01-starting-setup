02fd0631d5a4d0f90d0809924071a2346127c173
KrasiG <krasi@indexbg.bg>
2025-10-08 23:40:56 +0300

    button

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import BaseCard from './componends/UI/BaseCard.vue';
+import BaseButton from './componends/UI/BaseButton.vue';
 
 const app=createApp(App);
 app.component('BaseCard',BaseCard);
+app.component('BaseButton',BaseButton);
 
 if (process.env.NODE_ENV === 'development') {
   app.config.devtools = true
